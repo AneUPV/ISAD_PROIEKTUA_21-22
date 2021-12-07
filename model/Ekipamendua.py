9424ddee9154863a8594ffb9360a6a38a3c00fdd
AneUPV <90972402+AneUPV@users.noreply.github.com>
2021-12-07 18:30:18 +0100

    Delete model directory

--- model/Ekipamendua.py
+++
@@ -1,7 +0,0 @@
-
-class Ekipamendua:
-
-    def __init__(self, izena, guztira):
-
-        self.izena = izena
-        self.guztira = guztira      # Guztira aldagaian, ekipamendu horrekin egindako kilometro kopurua gordeko da

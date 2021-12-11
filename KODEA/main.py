6f5a0d31fdd3086b35805b02930c03850a50fec2
AneUPV <90972402+AneUPV@users.noreply.github.com>
2021-12-11 17:04:20 +0100

    BERTSIO FINALA

---
+++ KODEA/main.py
@@ -0,0 +1,43 @@
+from controllers.StravaAPI import stravaApiKud
+from controllers.DBKudeatzailea.DBKudeatzailea import DBKudeatzailea
+from view.Leihoa import Leihoa
+
+if __name__ == '__main__':
+
+# -------------------------------------------------------------#
+#                   API-KO DEIAK                               #
+# -------------------------------------------------------------#
+
+    #stravaApiKud.getAccessToTheAPI()
+
+    #1. /athlete
+    #stravaApiKud.getAthlete()
+
+    #2. /athlete/activities
+    #ema=stravaApiKud.getActivities()
+    #id=ema[1]["id"]
+
+    #3. /activities/%id
+    #stravaApiKud.getActivities_id(id)
+
+    #4. /activities/%id/streams
+    #stravaApiKud.getActivities_id_streams(id)
+# -------------------------------------------------------------#
+#                   DEI LAGUNGARRIAK                           #
+# -------------------------------------------------------------#
+
+    dbk = DBKudeatzailea()
+    #DB-ko taulak sortu
+    #dbk.taulakSortu()
+
+    #Stravako API-tik datuak atera eta DB-an sartu
+    #dbk.datuakKargatu()
+
+# -------------------------------------------------------------#
+#                   LEHIOAREN DEIA                             #
+# -------------------------------------------------------------#
+
+    Leihoa()
+
+
+
---
+++ KODEA/model/Ekipamendua.py
@@ -0,0 +1,7 @@
+
+class Ekipamendua:
+
+    def __init__(self, izena, guztira):
+
+        self.izena = izena
+        self.guztira = guztira      # Guztira aldagaian, ekipamendu horrekin egindako kilometro kopurua gordeko da
